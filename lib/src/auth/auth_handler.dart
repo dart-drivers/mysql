f038067f64d8a62f698e8916ce8a6089cb08a70b
James Ots <code@jamesots.com>
2016-05-30 14:21:36 +0100

    Update usage of crypto library

--- lib/src/auth/auth_handler.dart
+++ lib/src/auth/auth_handler.dart
@@ -28,18 +28,11 @@ class _AuthHandler extends _Handler {
     if (_password == null) {
       hash = <int>[];
     } else {
-      var hasher = new SHA1();
-      hasher.add(UTF8.encode(_password));
-      var hashedPassword = hasher.close();
-
-      hasher = new SHA1();
-      hasher.add(hashedPassword);
-      var doubleHashedPassword = hasher.close();
-
-      hasher = new SHA1();
-      hasher.add(_scrambleBuffer);
-      hasher.add(doubleHashedPassword);
-      var hashedSaltedPassword = hasher.close();
+      var hashedPassword = sha1.convert(UTF8.encode(_password)).bytes;
+      var doubleHashedPassword = sha1.convert(hashedPassword).bytes;
+      var hashedSaltedPassword = sha1
+          .convert([]..addAll(_scrambleBuffer)..addAll(doubleHashedPassword))
+          .bytes;
 
       hash = new List<int>(hashedSaltedPassword.length);
       for (var i = 0; i < hash.length; i++) {
